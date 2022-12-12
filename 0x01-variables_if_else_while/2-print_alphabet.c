a028696fd28021254a8292d75a812ca8c950d760
Nene <fachefavor@gmail.com>
2022-12-12 14:07:08 -0800

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,14 @@
-#include <stdio>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+			putchar("\n");
+			return (0);
+}
