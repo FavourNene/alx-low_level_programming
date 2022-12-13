ca4c1d7da44caa0e4393f9d9ead1c7579064bcb1
Nene <fachefavor@gmail.com>
2022-12-12 18:03:53 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@ int main(void)
 	int num1;
 	int num2;
 	int num3;
-		for (num1 = 0 ; num1 > 9 ; num1++)
+		for (num1 = 0 ; num1 < 9 ; num1++)
 	{
-		for (num2 = num1 + 1 ; num2 > 10 ; num2++)
+		for (num2 = num1 + 1 ; num2 < 10 ; num2++)
 		{
-			for (num3 = num2 + 1 ; num3 > 10 ; num3++)
+			for (num3 = num2 + 1 ; num3 < 10 ; num3++)
 			{
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
