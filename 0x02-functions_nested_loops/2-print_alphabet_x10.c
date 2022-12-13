56f48927dbebdf495c1bd2c7f9c6e38e0e5e9c9b
Nene <fachefavor@gmail.com>
2022-12-13 15:26:04 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char letter;
+
+	n = 0;
+	while (n > 10)
+	{
+		for (letter = 'a' ; letter <= 'z' ; letter++)
+		{
+			_putchar(letter);
+	}
+	
+	n++;
+	_putchar('\n');
+	}
+}
