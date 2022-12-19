04b150973280359ad8366535aedec8c88fa7fef2
Nene <fachefavor@gmail.com>
2022-12-19 14:23:48 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,11 @@ void print_rev(char *s)
 {
 	int i;
 
-	while (i = 0; s[i] != '\0')
-	{
-		i++;
-
-	}
-
+	for (i = 0; s[i] != '\0'; i++)
+		;
 	for (i = i - 1; s[i] != '\0'; i--)
 	{
-		_putchar(s[index]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
