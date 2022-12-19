02db279708838efcbe328b90531d27a6f53d076d
Nene <fachefavor@gmail.com>
2022-12-19 15:20:59 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
  * print_array - a function that prints n elements of an array
  * @a: array name
  * @n: is the number of elements OF the array to be printed
- * Return: a and n inputs
+ * Return: (void)
  */
 void print_array(int *a, int n);
 {
