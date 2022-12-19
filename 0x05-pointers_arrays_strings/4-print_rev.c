e011bf8a7a788af2c9c23be093d5d09578f3ca32
Nene <fachefavor@gmail.com>
2022-12-19 12:15:08 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,4 @@
+#include "main.h"
+/**
+ * print_rev - function thet prints strings in reverse
+ * 
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character of a string
+ *  starting with the first character, followed by a new line
+ *  @str: string input 
+ *  Return: editted string
+ */
+void puts2(char *str)
+{
+	int i;
+	
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+       if ((i % 2) == 0)
+	{
+		_putchar(str[i]);
+	}
+       else
+	       continue;
+       _putchar('\n');
+}
+
+
