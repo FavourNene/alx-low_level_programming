909f0f357567a0fd6be104bb5980da177e5c3930
Nene <fachefavor@gmail.com>
2022-12-20 13:31:57 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	return (dest);
+	return (&dest[0]);
 
 }
