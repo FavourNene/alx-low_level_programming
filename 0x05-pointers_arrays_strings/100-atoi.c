3123a862df304cddc98b6113a9451dfa2f2e961e
Nene <fachefavor@gmail.com>
2022-12-20 14:03:39 -0800

    task 100 again

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,11 +15,13 @@ int _atoi(char *s)
 	do {
 		if (*s == '-')
 			sign *= -1;
+
 		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s _ '0');
+			num = (num * 10) + (*s - '0');
+
 		else if (num > 0)
 			break;
-	} while (s++);
+	} while (*s++);
 
 	return (num * sign);
 }
